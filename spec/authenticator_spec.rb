624b9335c2a9cf9c6b9a18933fc783ad460faf67
Brian V. Hughes <brianvh@dartmouth.edu>
2010-09-24 14:25:49 -0400

    Initial specs for the Authenticator class.
     * Needed to create an "app" object to be the fake application making the request.
     * Set the fake app to respond to "/", "/login" and "/logout" requests with
       different response codes and headers, which could be verified against.
     * Needed to require 'rack/mock' in the spec_helper so I can use the specialized
       calls for setup the mock environment and the mocked page request.

--- spec/authenticator_spec.rb
+++ spec/authenticator_spec.rb
@@ -1,9 +1,51 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-describe Rack::Webauth::Authenticator do
-  
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+module Rack ; module Webauth
+  describe Authenticator do
+
+    before(:each) do
+      @auth = Authenticator.new(app)
+    end
+
+    describe "basic operation" do
+      
+      it "should return a normal response from a standard call" do
+        env = Rack::MockRequest.env_for("/", 'rack.session' => {})
+        response = Rack::MockResponse.new(*@auth.call(env))
+        response.status.should == 200
+      end
+
+      it "should redirect to the login page when a login is requested" do
+        env = Rack::MockRequest.env_for('/login', 'rack.session' => {})
+        response = Rack::MockResponse.new(*@auth.call(env))
+        response.status.should == 302
+        response.headers['location'].should match(/login\.dartmouth\.edu\/cas\/login/)
+      end
+
+      it "should redirect to the logout page when a logout is requested" do
+        env = Rack::MockRequest.env_for('/logout', 'rack.session' => {})
+        response = Rack::MockResponse.new(*@auth.call(env))
+        response.status.should == 302
+        response.headers['location'].should match(/login\.dartmouth\.edu\/logout\.php/)
+      end
+    end
+
+    def app
+      proc do |env|
+        request = Rack::Request.new(env)
+        case request.path
+          when '/'
+            [200, { "Content-Type" => "text/plain" }, [request.session.inspect]]
+          when '/login'
+            env['rack.session'][:webauth][:login] = true
+            [200, { "Content-Type" => "text/plain" }, [request.session.inspect]]
+          when '/logout'
+            env['rack.session'][:webauth][:logout] = true
+            [200, { "Content-Type" => "text/plain" }, [request.session.inspect]]
+        end
+      end
+    end
+
   end
-  
-end
+
+end ; end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'spec'
 require 'spec/autorun'
 
+require 'rack/mock'
 require 'rack/webauth'
 
 Spec::Runner.configure do |config|
