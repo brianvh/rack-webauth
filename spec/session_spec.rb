4283bdbe15dbbb9bb376b2a97a4530231496cb0a
Brian V. Hughes <brianvh@mac.com>
2010-09-25 17:52:32 -0400

    Moved the check for env['rack.session'] to the Session class.
     * Modified the main webauth.rb namespace to include the Session class
     * Added a spec to verify that Session throws an error, if rack.session isn't set
     * Moved the rack.session check statement from the Authenticator call method

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -2,4 +2,9 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe Rack::Webauth::Session do
 
+  it "should raise an error if rack.session isn't set" do
+    env = Rack::MockRequest.env_for("/")
+    lambda { Rack::Webauth::Session.new(env)}.should raise_error("Rack::Webauth requires Rack::Session.")
+  end
+
 end
